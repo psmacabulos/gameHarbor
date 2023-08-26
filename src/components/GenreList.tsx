76d69439e7ed30d29bd2663a725f8004fc33e664
psmacabulos <107623965+psmacabulos@users.noreply.github.com>
2023-08-26 18:35:23 +0800

    Implement spinner on sidebar

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -1,24 +1,28 @@
-import { HStack, Image, List, ListItem, Text } from "@chakra-ui/react"
+import { HStack, Image, List, ListItem, Spinner, Text } from "@chakra-ui/react"
 import useGenres from "../hooks/useGenres"
 import getOptimizedImageUrl from "../services/imageUrl"
 
 const GenreList = () => {
-  const { data } = useGenres()
+  const { data, error, isLoading } = useGenres()
+  if (error) return null
   return (
-    <List>
-      {data.map(genre => (
-        <ListItem key={genre.id} paddingY='5px'>
-          <HStack>
-            <Image
-              src={getOptimizedImageUrl(genre.image_background)}
-              boxSize='32px'
-              borderRadius='8px'
-            />
-            <Text fontSize='lg'>{genre.name}</Text>
-          </HStack>
-        </ListItem>
-      ))}
-    </List>
+    <>
+      {isLoading && <Spinner size='xl' />}
+      <List>
+        {data.map(genre => (
+          <ListItem key={genre.id} paddingY='5px'>
+            <HStack>
+              <Image
+                src={getOptimizedImageUrl(genre.image_background)}
+                boxSize='32px'
+                borderRadius='8px'
+              />
+              <Text fontSize='lg'>{genre.name}</Text>
+            </HStack>
+          </ListItem>
+        ))}
+      </List>
+    </>
   )
 }
 
